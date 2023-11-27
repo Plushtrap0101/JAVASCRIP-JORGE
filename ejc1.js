16ca8dbdd6df3f7cfe7f093b56664f78e8ecd0ae
Plushtrap0101 <cardonaesteban0101@gmail.com>
2023-11-27 17:19:42 -0500

    27-11-2023

--- ejc1.js
+++ ejc1.js
@@ -2,7 +2,7 @@ function agregarImg(){
     let contador = 0;
     while(contador < 20){
     
-    let imagen =`mascota${contador},jpg`;
+    let imagen =`mascota${contador}.jpg`;
     let contenedor = document.getElementById(`${contador}`);
     contenedor.insertAdjacentHTML("beforeend",`<img src=${imagen} alt=${imagen} class="imgBody">`)
     contador++;
