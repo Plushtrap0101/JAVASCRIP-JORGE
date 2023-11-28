9a2a0b17d98b376a44496d674896aee6539cbf48
Plushtrap0101 <cardonaesteban0101@gmail.com>
2023-11-28 17:14:34 -0500

    28-11-2023

--- ejc1.js
+++ ejc1.js
@@ -2,9 +2,16 @@ function agregarImg(){
     let contador = 0;
     while(contador < 20){
     
-    let imagen =`mascota${contador}.jpg`;
+    let imagen =`juego${contador}.jpg`;
     let contenedor = document.getElementById(`${contador}`);
     contenedor.insertAdjacentHTML("beforeend",`<img src=${imagen} alt=${imagen} class="imgBody">`)
     contador++;
+    }
+}
+function eliminarImg(){
+    for(let i = 0; <= 20; i++){
+        let delete=document.getElementById(`${contador}`);
+        delete.remove();
+        cont = 1;
     }
 }
